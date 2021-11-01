b05e8b192a8b3188432f63e0b244b7058e930495
danieltran-97 <danielmctran@gmail.com>
2021-11-01 16:08:23 +1100

    stubs

--- StockAnalyzerTest/StockAnalyzerTest.cs
+++ StockAnalyzerTest/StockAnalyzerTest.cs
@@ -1,4 +1,5 @@
 using System;
+using Moq;
 using Xunit;
 
 namespace StockAnalyzer
@@ -7,8 +8,12 @@ namespace StockAnalyzer
     {
         [Fact]
         public void TestStockAnalyzerReturnsStockPrice() {
-            IStockFeed stubStockFeed = new StubStockFeed();
-            StockAnalyzer itemUnderTest = new StockAnalyzer(stubStockFeed);
+            // IStockFeed stubStockFeed = new StubStockFeed();
+            Mock<IStockFeed> stubStockFeed = new Mock<IStockFeed>();
+            stubStockFeed.Setup(x => x.GetSharePrice(It.IsAny<string>())).Returns(100);
+            
+            StockAnalyzer itemUnderTest = new StockAnalyzer(stubStockFeed.Object);
+            // StockAnalyzer itemUnderTest = new StockAnalyzer(stubStockFeed);
 
             Assert.Equal(100, itemUnderTest.GetMyobPrice());
         }
